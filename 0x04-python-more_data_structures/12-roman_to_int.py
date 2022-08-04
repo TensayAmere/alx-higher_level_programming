d4332707dfa843b4fc1618d8287eb2b7cdf756ef
TensayAmere <101726216+TensayAmere@users.noreply.github.com>
2022-08-04 21:36:14 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,33 +1,45 @@
 #!/usr/bin/python3
-# 12-roman_to_int.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
+    result = 0
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    repeat_count = 0
 
-    roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-    }
-    num = 0
+    if roman_string is None:
+        return 0
+    if not isinstance(roman_string, str):
+        return 0
+    if len(roman_string) is None:
+        return 0
+    for x in roman_string:
+        if x not in romans:
+            return 0
+    if len(roman_string) == 1:
+        return romans[roman_string]
 
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
+    # Make a list of each letter in the string and the one following it.
+    # If the current roman numeral is repeated before a larger roman numeral,
+    # return 0.
+    for i, n in zip(roman_string, roman_string[1:]):
 
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
+        if romans[i] > romans[n]:
+            result += romans[i]
+            repeat_count = 0
 
-        else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+        elif romans[i] is romans[n]:
+            if repeat_count > 3:
+                return 0
+            result += romans[i]
+            repeat_count += 1
+
+        else:  # romans[i] < romans[n]:
+            if repeat_count > 0:
+                return 0
+            result -= romans[i]
+            repeat_count = 0
+
+    # add the last letter of the string which couldn't be zip'd
+    if repeat_count > 3:
+        return 0
+    result += romans[roman_string[-1]]
+
+    return result
